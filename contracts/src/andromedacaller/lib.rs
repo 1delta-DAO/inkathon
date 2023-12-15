30ad18bf95d18da486a8fe23c452e19c80fa05e1
caglavol <volkan@1delta.io>
2023-12-15 17:41:04 +0100

    fix liquidity pool creation and add swap function

--- contracts/src/andromedacaller/lib.rs
+++ contracts/src/andromedacaller/lib.rs
@@ -8,31 +8,11 @@ mod andromeda {
 mod andromedacaller {
 
     use crate::andromeda::traits::{PSP22TradingPair, Router, PSP22};
-    use ink::{contract_ref, prelude::vec::Vec, LangError};
+    use ink::{contract_ref, prelude::vec::Vec};
     use openbrush::contracts::psp22::PSP22Error;
-
-    #[derive(Debug, scale::Encode, scale::Decode)]
-    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-    pub enum CallerError {
-        PSP22(PSP22Error),
-        Lang(LangError),
-    }
-
-    impl From<PSP22Error> for CallerError {
-        fn from(error: PSP22Error) -> Self {
-            CallerError::PSP22(error)
-        }
-    }
-
-    impl From<LangError> for CallerError {
-        fn from(error: LangError) -> Self {
-            CallerError::Lang(error)
-        }
-    }
 
     #[ink(storage)]
     pub struct AndromedaCaller {
-        andromeda_account: AccountId,
         swap_router: contract_ref!(Router),
     }
 
@@ -40,61 +20,88 @@ mod andromedacaller {
         #[ink(constructor)]
         pub fn new(andromeda_router_address: AccountId) -> Self {
             Self {
-                andromeda_account: andromeda_router_address,
                 swap_router: andromeda_router_address.into(),
             }
         }
 
-        // lending_pool_deposit functions
         #[ink(message)]
         pub fn create_psp22_liquidity_pool(
             &mut self,
             psp22_token_a: AccountId,
             psp22_token_b: AccountId,
-            version: u64,
             token_a_amount: Balance,
             token_b_amount: Balance,
-            slippage: Balance,
-        ) -> Result<(), CallerError> {
+        ) -> Result<AccountId, PSP22Error> {
             let caller: AccountId = Self::env().caller();
+            const VERSION: u64 = 0;
+            const SLIPPAGE: Balance = 0;
 
-            // create liquidity pool
             let swap_pool_account_id =
                 self.swap_router
-                    .create_psp22_liquidity(psp22_token_a, psp22_token_b, version)?;
+                    .create_psp22_liquidity(psp22_token_a, psp22_token_b, VERSION);
 
-            // transfer tokens from caller to this contract
             let mut token_a: contract_ref!(PSP22) = psp22_token_a.into();
             token_a.transfer_from(caller, self.env().account_id(), token_a_amount, Vec::new())?;
 
             let mut token_b: contract_ref!(PSP22) = psp22_token_b.into();
             token_b.transfer_from(caller, self.env().account_id(), token_b_amount, Vec::new())?;
 
-            // deposit liquidity
             token_a.approve(swap_pool_account_id, token_a_amount)?;
             token_b.approve(swap_pool_account_id, token_b_amount)?;
 
             let mut swap_pool: contract_ref!(PSP22TradingPair) = swap_pool_account_id.into();
-            swap_pool.add_liquidity(token_a_amount, token_b_amount, slippage)?;
+            swap_pool.add_liquidity(token_a_amount, token_b_amount, SLIPPAGE);
 
-            // transfer liquidity tokens to caller
             let mut liquidity_token: contract_ref!(PSP22) = swap_pool_account_id.into();
             let liquidity_token_balance: u128 = liquidity_token.balance_of(self.env().account_id());
             liquidity_token.transfer(caller, liquidity_token_balance, Vec::new())?;
 
-            return Ok(());
+            return Ok(swap_pool_account_id);
         }
 
-        // PSP22 functions
         #[ink(message)]
-        pub fn approve(
-            &self,
-            asset: AccountId,
-            spender: AccountId,
-            value: u128,
-        ) -> Result<(), PSP22Error> {
-            let mut token: contract_ref!(PSP22) = asset.into();
-            token.approve(spender, value)
+        pub fn swap_psp22_tokens(
+            &mut self,
+            psp22_token_start: AccountId,
+            psp22_token_end: AccountId,
+            token_start_amount: Balance,
+            slippage: Balance,
+        ) -> Result<Balance, PSP22Error> {
+            let caller: AccountId = Self::env().caller();
+
+            let swap_pool_account_id = self
+                .swap_router
+                .get_associated_psp22_pool(psp22_token_start, psp22_token_end);
+
+            let mut token_start: contract_ref!(PSP22) = psp22_token_start.into();
+            token_start.transfer_from(
+                caller,
+                self.env().account_id(),
+                token_start_amount,
+                Vec::new(),
+            )?;
+
+            token_start.approve(swap_pool_account_id, token_start_amount)?;
+
+            let mut swap_pool: contract_ref!(PSP22TradingPair) = swap_pool_account_id.into();
+            let token_ids: Vec<AccountId> = swap_pool.get_tokens();
+
+            let token_end_amount: u128;
+            if token_ids[0] == psp22_token_start {
+                token_end_amount = swap_pool.get_psp22_b_amount_out(token_start_amount);
+
+                swap_pool.swap_psp22_token_a(token_start_amount, token_end_amount, slippage);
+            } else {
+                token_end_amount = swap_pool.get_psp22_a_amount_out(token_start_amount);
+
+                swap_pool.swap_psp22_token_b(token_start_amount, token_end_amount, slippage);
+            }
+
+            let mut token_end: contract_ref!(PSP22) = psp22_token_end.into();
+            let token_end_amount_realised: u128 = token_end.balance_of(self.env().account_id());
+            token_end.transfer(caller, token_end_amount_realised, Vec::new())?;
+
+            return Ok(token_end_amount_realised);
         }
     }
 }
