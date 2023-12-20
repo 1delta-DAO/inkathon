0ff3f11da62710664d4ff7485966ca4a513caaf6
caglavol <volkan@1delta.io>
2023-12-20 12:42:19 +0100

    move abax assets to separate file

--- contracts/test/abaxcaller/abaxcaller.test.ts
+++ contracts/test/abaxcaller/abaxcaller.test.ts
@@ -2,6 +2,7 @@ import { ApiPromise, WsProvider } from '@polkadot/api'
 import { ContractPromise } from '@polkadot/api-contract'
 import { Keyring } from '@polkadot/keyring'
 import { IKeyringPair } from '@polkadot/types/types'
+import { ASSETS } from '../../data/assets'
 import callerAbiPath from '../../deployments/abaxcaller/abaxcaller.json'
 import { address } from '../../deployments/abaxcaller/development'
 import psp22AbiPath from '../../deployments/psp22/psp22.json'
@@ -35,45 +36,6 @@ const BALANCES = {
   BTC: 500000000n,
   AZERO: 100000000000000000n,
   DOT: 20000000000000000n,
-}
-
-const ASSETS = {
-  DAI: {
-    address: '5ELYqHS8YZ2hAEnCiqGJg8Ztc6JoFFKHpvgUbuz9oW9vc5at',
-    aTokenAddress: '5GusvrnNEfYThkDxdSUZRca9ScTiVyrF3S76UJEQTUXBDdmT',
-    vTokenAddress: '5EkScoCUiXCraQw5kSbknbugVEhWod9xMv4PRkyo9MHTREXw',
-    decimals: 6,
-  },
-  USDC: {
-    address: '5GXDPgrjJC7cyr9B1jCm5UqLGuephaEKGoAeHKfodB3TVghP',
-    aTokenAddress: '5EVfH2BRm2ggimfRcuEH8zRYkviyEN69et4fDjHWHzWjirBK',
-    vTokenAddress: '5CdF6Vdf9mAG5fjhFuNaLfFLj9i31SjxBsVj5JHBARmL5Xmd',
-    decimals: 6,
-  },
-  WETH: {
-    address: '5DgMoQHDKSJryNGR4DXo5H267Hmnf9ph5ZMLPXBtPxcZfN3P',
-    aTokenAddress: '5DvMrRU79zS29FSSP5k8CyuCK2de59Xvvqwzbm1UzqNWwxFY',
-    vTokenAddress: '5HY4mmPQuMDakTDeaf6Cj5TJaSbmb7G3fHczcyuyhmU6UeVR',
-    decimals: 18,
-  },
-  BTC: {
-    address: '5CJCSzTY2wZQaDp9PrzC1LsVfTEp9sGBHcAY3vjv9JLakfX9',
-    aTokenAddress: '5GZm7bsGE53Gyf9Cg2GwsTjDrP9skY6A6uSZiCFWDoEZyMtj',
-    vTokenAddress: '5EEurzNsm5SMDSJBHbtu4GHbkdSsHdjPbWNb28vxpEWkZJWX',
-    decimals: 8,
-  },
-  AZERO: {
-    address: '5CLLmNswXre58cuz6hBnyscpieFYUyqq5vwvopiW3q41SSYF',
-    aTokenAddress: '5Da8px1HEoAvs3m9i55ftfSswDbskqCY4rHr1KAFsTqfiTia',
-    vTokenAddress: '5ChJnTSpsQ26zJGyGt7uHHLRjbquAy1JSmaijMujUi9VKfJL',
-    decimals: 12,
-  },
-  DOT: {
-    address: '5EwcHvcGBC9jnVzmPJUzwgZJLxUkrWCzzZfoqpjZ45o9C9Gh',
-    aTokenAddress: '5D1dwQEhyXzVDuB8RX85xm9iNa4pTtUr2jVpYHNFte7FxRTw',
-    vTokenAddress: '5HDidr2RT4VGkxyGuJieGAfqYpqphwviB4WULaNp6VNsf2B2',
-    decimals: 12,
-  },
 }
 
 describe('Abaxcaller contract interactions', () => {
