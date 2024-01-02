94a5aa988f842869ee9e05c3c0a94b30e0fcd316
caglavol <volkan@1delta.io>
2024-01-02 16:57:35 +0100

    refactor abaxcaller test suite

--- contracts/test/abaxcaller/abaxcaller.test.ts
+++ contracts/test/abaxcaller/abaxcaller.test.ts
@@ -1,4 +1,5 @@
 import { ApiPromise, WsProvider } from '@polkadot/api'
+import { ContractPromise } from '@polkadot/api-contract'
 import { Keyring } from '@polkadot/keyring'
 import { KeyringPair } from '@polkadot/keyring/types'
 import { BN } from '@polkadot/util'
@@ -29,28 +30,40 @@ const MAXUINT128 = new BN(2).pow(new BN(128)).sub(new BN(1))
 describe('Abaxcaller contract interactions', () => {
   let api: ApiPromise
   let account: KeyringPair
-  let abaxCaller: AbaxCaller
   let abax: Abax
+  let callAbaxCaller: (functionName: string, args: any[], caller?: KeyringPair) => Promise<any>
   let callAbax: (functionName: string, args: any[], caller?: KeyringPair) => Promise<any>
-  
+  let callContract: (
+    contract: ContractPromise,
+    functionName: string,
+    args: any[],
+    caller?: KeyringPair,
+  ) => Promise<any>
+
   beforeAll(async () => {
     const provider = new WsProvider(ENDPOINT)
     api = await ApiPromise.create({ provider })
     account = new Keyring({ type: 'sr25519' }).addFromUri('//Alice', { name: 'Alice' })
 
-    abaxCaller = new AbaxCaller(address, account, api)
     abax = new Abax(ABAX_ADDRESS, account, api)
+    const abaxCaller = new AbaxCaller(address, account, api)
 
-    // call abax protocol from account via name and args
-    callAbax = async (functionName: string, args: any[], caller?: KeyringPair) => await contractTx(
-      api,
-      caller ?? account,
-      abax.nativeContract,
-      functionName,
-      args,
-    ).catch((error) => {
-      console.error('Set collateral transaction error:', error)
-    })
+    callContract = async (
+      contract: ContractPromise,
+      functionName: string,
+      args: any[],
+      caller: KeyringPair,
+    ) => {
+      await contractTx(api, caller ?? account, contract, functionName, args).catch((error) => {
+        console.error(functionName, error)
+      })
+    }
+
+    callAbax = async (functionName: string, args: any[], caller?: KeyringPair) =>
+      await callContract(abax.nativeContract, functionName, args, caller)
+
+    callAbaxCaller = async (functionName: string, args: any[], caller?: KeyringPair) =>
+      await callContract(abaxCaller.nativeContract, functionName, args, caller)
   })
 
   afterAll(async () => {
@@ -65,32 +78,16 @@ describe('Abaxcaller contract interactions', () => {
 
       // max approve: owner = AbaxCaller, spender = AbaxLendingPool
       const maxApproveArgs = [ASSETS[asset].address, ABAX_ADDRESS, MAXUINT128]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_APPROVE,
-        maxApproveArgs,
-      ).catch((error) => {
-        console.error('Max approve transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_APPROVE, maxApproveArgs)
 
       // approve for deposit: owner = account, spender = AbaxCaller
       const token = new PSP22(ASSETS[asset].address, account, api)
       const approveDepositArgs = [address, depositAmount]
-      await contractTx(
-        api,
-        account,
-        token.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveDepositArgs,
-      ).catch((error) => {
-        console.error('Approve deposit transaction error:', error)
-      })
+      await callContract(token.nativeContract, TOKEN_FUNCTION_APPROVE, approveDepositArgs)
 
       // call deposit function in AbaxCaller contract
       const depositArgs = [ASSETS[asset].address, depositAmount, []]
-      await callAbax(CALLER_FUNCTION_DEPOSIT, depositArgs)
+      await callAbaxCaller(CALLER_FUNCTION_DEPOSIT, depositArgs)
 
       // view user reserve data for verification
       const reserveData = (
@@ -115,64 +112,28 @@ describe('Abaxcaller contract interactions', () => {
 
       // max approve: owner = AbaxCaller, spender = AbaxLendingPool
       const maxApproveArgs = [ASSETS[asset].address, ABAX_ADDRESS, MAXUINT128]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_APPROVE,
-        maxApproveArgs,
-      ).catch((error) => {
-        console.error('Max approve transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_APPROVE, maxApproveArgs)
 
       // approve for deposit: owner = account, spender = AbaxCaller
       const approveDepositArgs = [address, depositAmount]
-      await contractTx(
-        api,
-        account,
-        token.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveDepositArgs,
-      ).catch((error) => {
-        console.error('Approve deposit transaction error:', error)
-      })
+      await callContract(token.nativeContract, TOKEN_FUNCTION_APPROVE, approveDepositArgs)
 
       // call deposit function in AbaxCaller contract
       const depositArgs = [ASSETS[asset].address, depositAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_DEPOSIT,
-        depositArgs,
-      ).catch((error) => {
-        console.error('Deposit transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_DEPOSIT, depositArgs)
 
       // approve collateral token for withdrawal: owner = account, spender = AbaxCaller
       const approveCollateralTokenArgs = [address, withdrawAmount]
       const collateralToken = new PSP22(ASSETS[asset].aTokenAddress, account, api)
-      await contractTx(
-        api,
-        account,
+      await callContract(
         collateralToken.nativeContract,
         TOKEN_FUNCTION_APPROVE,
         approveCollateralTokenArgs,
-      ).catch((error) => {
-        console.error('Approve collateral transaction error:', error)
-      })
+      )
 
       // call withdraw function in AbaxCaller contract
       const withdrawArgs = [ASSETS[asset].address, withdrawAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_REDEEM,
-        withdrawArgs,
-      ).catch((error) => {
-        console.error('Withdraw transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_REDEEM, withdrawArgs)
 
       // view token balance for verification
       const realizedAmount = (await token.query.balanceOf(account.address)).value.unwrap().rawNumber
@@ -195,76 +156,28 @@ describe('Abaxcaller contract interactions', () => {
 
       // max approve: owner = AbaxCaller, spender = AbaxLendingPool
       const maxApproveArgs = [ASSETS[asset].address, ABAX_ADDRESS, MAXUINT128]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_APPROVE,
-        maxApproveArgs,
-      ).catch((error) => {
-        console.error('Max approve transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_APPROVE, maxApproveArgs)
 
       // approve for deposit: owner = account, spender = AbaxCaller
       const approveDepositArgs = [address, depositAmount]
-      await contractTx(
-        api,
-        account,
-        token.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveDepositArgs,
-      ).catch((error) => {
-        console.error('Approve deposit transaction error:', error)
-      })
+      await callContract(token.nativeContract, TOKEN_FUNCTION_APPROVE, approveDepositArgs)
 
       // call deposit function in AbaxCaller contract
       const depositArgs = [ASSETS[asset].address, depositAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_DEPOSIT,
-        depositArgs,
-      ).catch((error) => {
-        console.error('Deposit transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_DEPOSIT, depositArgs)
 
       // approve for borrow: owner = account, spender = AbaxCaller
       const debtToken = new PSP22(ASSETS[asset].vTokenAddress, account, api)
       const approveBorrowArgs = [address, borrowAmount]
-      await contractTx(
-        api,
-        account,
-        debtToken.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveBorrowArgs,
-      ).catch((error) => {
-        console.error('Approve borrow transaction error:', error)
-      })
+      await callContract(debtToken.nativeContract, TOKEN_FUNCTION_APPROVE, approveBorrowArgs)
 
       // set deposit as collateral
       const setCollateralArgs = [ASSETS[asset].address, true]
-      await contractTx(
-        api,
-        account,
-        abax.nativeContract,
-        ABAX_FUNCTION_SET_AS_COLLATERAL,
-        setCollateralArgs,
-      ).catch((error) => {
-        console.error('Set collateral transaction error:', error)
-      })
+      await callAbax(ABAX_FUNCTION_SET_AS_COLLATERAL, setCollateralArgs)
 
       // call borrow function in AbaxCaller contract
       const borrowArgs = [ASSETS[asset].address, borrowAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_BORROW,
-        borrowArgs,
-      ).catch((error) => {
-        console.error('Borrow transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_BORROW, borrowArgs)
 
       // view token balance for verification
       const realizedAmount = (await token.query.balanceOf(account.address)).value.unwrap().rawNumber
@@ -288,100 +201,36 @@ describe('Abaxcaller contract interactions', () => {
 
       // max approve: owner = AbaxCaller, spender = AbaxLendingPool
       const maxApproveArgs = [ASSETS[asset].address, ABAX_ADDRESS, MAXUINT128]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_APPROVE,
-        maxApproveArgs,
-      ).catch((error) => {
-        console.error('Max approve transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_APPROVE, maxApproveArgs)
 
       // approve for deposit: owner = account, spender = AbaxCaller
       const approveDepositArgs = [address, depositAmount]
-      await contractTx(
-        api,
-        account,
-        token.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveDepositArgs,
-      ).catch((error) => {
-        console.error('Approve deposit transaction error:', error)
-      })
+      await callContract(token.nativeContract, TOKEN_FUNCTION_APPROVE, approveDepositArgs)
 
       // call deposit function in AbaxCaller contract
       const depositArgs = [ASSETS[asset].address, depositAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_DEPOSIT,
-        depositArgs,
-      ).catch((error) => {
-        console.error('Deposit transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_DEPOSIT, depositArgs)
 
       // approve for borrow: owner = account, spender = AbaxCaller
       const debtToken = new PSP22(ASSETS[asset].vTokenAddress, account, api)
       const approveBorrowArgs = [address, borrowAmount]
-      await contractTx(
-        api,
-        account,
-        debtToken.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveBorrowArgs,
-      ).catch((error) => {
-        console.error('Approve borrow transaction error:', error)
-      })
+      await callContract(debtToken.nativeContract, TOKEN_FUNCTION_APPROVE, approveBorrowArgs)
 
       // set deposit as collateral
       const setCollateralArgs = [ASSETS[asset].address, true]
-      await contractTx(
-        api,
-        account,
-        abax.nativeContract,
-        ABAX_FUNCTION_SET_AS_COLLATERAL,
-        setCollateralArgs,
-      ).catch((error) => {
-        console.error('Set collateral transaction error:', error)
-      })
+      await callAbax(ABAX_FUNCTION_SET_AS_COLLATERAL, setCollateralArgs)
 
       // call borrow function in AbaxCaller contract
       const borrowArgs = [ASSETS[asset].address, borrowAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_BORROW,
-        borrowArgs,
-      ).catch((error) => {
-        console.error('Borrow transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_BORROW, borrowArgs)
 
       // approve for repay: owner = account, spender = AbaxCaller
       const approveRepayArgs = [address, repayAmount]
-      await contractTx(
-        api,
-        account,
-        token.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveRepayArgs,
-      ).catch((error) => {
-        console.error('Approve repay transaction error:', error)
-      })
+      await callContract(token.nativeContract, TOKEN_FUNCTION_APPROVE, approveRepayArgs)
 
       // call repay function in AbaxCaller contract
       const repayArgs = [ASSETS[asset].address, repayAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_REPAY,
-        repayArgs,
-      ).catch((error) => {
-        console.error('Repay transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_REPAY, repayArgs)
 
       // view token balance for verification
       const realizedAmount = (await token.query.balanceOf(account.address)).value.unwrap().rawNumber
@@ -401,78 +250,30 @@ describe('Abaxcaller contract interactions', () => {
 
       // max approve: owner = AbaxCaller, spender = AbaxLendingPool
       const maxApproveArgs = [ASSETS[asset].address, ABAX_ADDRESS, MAXUINT128]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_APPROVE,
-        maxApproveArgs,
-      ).catch((error) => {
-        console.error('Max approve transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_APPROVE, maxApproveArgs)
 
       // approve for deposit: owner = account, spender = AbaxCaller
       const token = new PSP22(ASSETS[asset].address, account, api)
       const approveDepositArgs = [address, depositAmount]
-      await contractTx(
-        api,
-        account,
-        token.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveDepositArgs,
-      ).catch((error) => {
-        console.error('Approve deposit transaction error:', error)
-      })
+      await callContract(token.nativeContract, TOKEN_FUNCTION_APPROVE, approveDepositArgs)
 
       // call deposit function in AbaxCaller contract
       const depositArgs = [ASSETS[asset].address, depositAmount, []]
-      await contractTx(
-        api,
-        account,
-        abaxCaller.nativeContract,
-        CALLER_FUNCTION_DEPOSIT,
-        depositArgs,
-      ).catch((error) => {
-        console.error('Deposit transaction error:', error)
-      })
+      await callAbaxCaller(CALLER_FUNCTION_DEPOSIT, depositArgs)
 
       // approve for borrow in flash Loan: owner = account, spender = AbaxCaller
       const debtToken = new PSP22(ASSETS[asset].vTokenAddress, account, api)
       const approveBorrowArgs = [address, flashLoanAmount]
-      await contractTx(
-        api,
-        account,
-        debtToken.nativeContract,
-        TOKEN_FUNCTION_APPROVE,
-        approveBorrowArgs,
-      ).catch((error) => {
-        console.error('Approve borrow transaction error:', error)
-      })
+      await callContract(debtToken.nativeContract, TOKEN_FUNCTION_APPROVE, approveBorrowArgs)
 
       // set deposit as collateral
       const setCollateralArgs = [ASSETS[asset].address, true]
-      await contractTx(
-        api,
-        account,
-        abax.nativeContract,
-        ABAX_FUNCTION_SET_AS_COLLATERAL,
-        setCollateralArgs,
-      ).catch((error) => {
-        console.error('Set collateral transaction error:', error)
-      })
+      await callAbax(ABAX_FUNCTION_SET_AS_COLLATERAL, setCollateralArgs)
 
       // call flash loan function in AbaxCaller contract
       const encodedAccountId = api.createType('AccountId', account.address).toHex()
       const flashLoanArgs = [address, [ASSETS[asset].address], [flashLoanAmount], encodedAccountId]
-      await contractTx(
-        api,
-        account,
-        abax.nativeContract,
-        ABAX_FUNCTION_FLASH_LOAN,
-        flashLoanArgs,
-      ).catch((error) => {
-        console.error('Flash loan transaction error:', error)
-      })
+      await callAbax(ABAX_FUNCTION_FLASH_LOAN, flashLoanArgs)
 
       // view token balance for verification
       const realizedAmount = (await debtToken.query.balanceOf(account.address)).value.unwrap()
